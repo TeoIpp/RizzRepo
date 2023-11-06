40168fe67010956ff05d68c32b6ede8a4e99da13
TeoIpp <145074065+TeoIpp@users.noreply.github.com>
2023-11-06 09:12:54 -0800

    Initial workspace file upload

---
+++ mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <QImage> // designed and optimized for I/O, and for direct pixel access and manipulation,
+#include <QPixmap> // designed and optimized for showing images on screen.
+#include <QBitmap> // convenience class that inherits QPixmap, ensuring a depth of 1.
+#include <QPicture> // paint device that records and replays QPainter commands.
+
+#include <string>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    QPixmap labelImage("/Users/mggirsby/Desktop/Jazz.jpg"); // add filepath to your own image here
+    ui->label->setPixmap(labelImage);
+    ui->label->setScaledContents(true);
+}
+
